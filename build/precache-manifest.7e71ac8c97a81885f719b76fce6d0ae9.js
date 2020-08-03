02d33aec6f64d7799a59d9f271f2ea4bd23c03aa
andredavid1 <andredavid1@yahoo.com.br>
2020-08-03 16:25:55 -0300

    build

---
+++ build/precache-manifest.7e71ac8c97a81885f719b76fce6d0ae9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64502e6bab083f8eaa6bd3358756af9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e1e4d190a3b3695dd27",
+    "url": "/static/css/main.35d3b31e.chunk.css"
+  },
+  {
+    "revision": "2c031f71a3f2c028b2e6",
+    "url": "/static/js/2.60d48aa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.60d48aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1e4d190a3b3695dd27",
+    "url": "/static/js/main.aae65fa0.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "156102ee75a899123483211bbf4a8fbd",
+    "url": "/static/media/give-classes.156102ee.svg"
+  },
+  {
+    "revision": "211f2bb00860f91804cfb2354e17b255",
+    "url": "/static/media/landing.211f2bb0.svg"
+  },
+  {
+    "revision": "86fef799b0726c66fb7a9d710e2d922c",
+    "url": "/static/media/logo.86fef799.svg"
+  },
+  {
+    "revision": "bfdb3ebe513b5b2411a5014d6aad3abe",
+    "url": "/static/media/purple-heart.bfdb3ebe.svg"
+  },
+  {
+    "revision": "e7a7e1b4e016245fa97e6f42aee4bcaa",
+    "url": "/static/media/study.e7a7e1b4.svg"
+  }
+]);
